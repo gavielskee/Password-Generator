7e7c5416d6df1f4e3702791b9046237bb0946e9e
gavielskee <gavielskee1@gmail.com>
2023-07-20 21:46:26 -0400

    defined characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.query
+
+//Password length
+var length = 10;Selector("#generate");
+
+//Main function
+function writePassword() {
+
 
 // Write password to the #password input
-function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,4 +18,11 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
+
+//Defining characters
+const lowerCase = "abcdefghijklmnopqrstuvqxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "01234567890";
+const specialChars = "!@#$%^&*()-_=+[{]}|;:,<.>?";
